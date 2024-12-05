0c482142fda7df2f0661ad67ba8ef61381fb3a92
yeji <130373396+skwldwld@users.noreply.github.com>
2024-12-06 00:44:25 +0900

    Title: 특정 조건의 수 뽑기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241206/특정 조건의 수 뽑기/draw-number-of-specific-conditions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int num[n] = {};
+    int near = 200;
+    int distance = 10;
+    int minnum = 200;
+
+    for(int i = 0; i < n; i++) {
+        cin >> num[i];
+        if(abs(100 - num[i]) < distance) {
+            near = num[i];
+            distance = 100 - num[i];
+        }
+        if(num[i] >= 100 && num[i] < minnum)
+            minnum = num[i];
+    }
+    cout << near << " " << minnum;
+    return 0;
+}
